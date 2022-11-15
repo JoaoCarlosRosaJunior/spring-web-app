6f9d55d9aae7380190bd76af641252c523aa65cd
joao.rosa <joao.rosa@idwall.co>
2022-11-15 13:43:55 -0300

    feat: project structure created

---
+++ backend/src/test/java/com/springapp/metasales/MetasalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.metasales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
