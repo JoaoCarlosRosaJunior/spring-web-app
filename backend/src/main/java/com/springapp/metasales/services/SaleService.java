aec0b84202ceed5ada5a214b7ed523b623bce31e
joao.rosa <joao.rosa@idwall.co>
2022-11-17 22:42:45 -0300

    feat: Sms service

---
+++ backend/src/main/java/com/springapp/metasales/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.springapp.metasales.services;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.springapp.metasales.entities.Sale;
+import com.springapp.metasales.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(7) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
+	}
+}
